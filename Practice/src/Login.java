9b44bf66fa63a916f7a8ab7e376339db87bdb6dc
abc <abc@gmail.com>
2017-09-25 11:19:08 +0530

    fisrt

---
+++ Practice/src/Login.java
@@ -0,0 +1,36 @@
+
+
+import java.net.URI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+
+public class Login {
+
+	
+	public static void main(String[] args) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.ie.driver","C:\\Program Files\\Internet Explorer\\iexplore.exe");
+		URI url = new URI("https://www.google.com");
+		java.awt.Desktop.getDesktop().browse(url);
+		
+		
+		
+		//WebDriver driver = new InternetExplorerDriver();
+		//String url = "https://www.google.com";
+		//driver.get(url);
+		
+		//driver.manage().window().maximize();
+		
+		
+		
+	}
+
+	
+	
+}
+	
